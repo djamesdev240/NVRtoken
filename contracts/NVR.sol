64259279115b3a6d8960da868403dd9714c4b8ec
anoraxdev <ellerandreas@protonmail.com>
2021-11-13 18:14:14 -0600

    Added initial rules: it should let owners mint, prevent non-owners from minting, and send a portion of the minted tokens to the founders accounts

--- contracts/NVR.sol
+++ contracts/NVR.sol
@@ -5,13 +5,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NVR is ERC20, Ownable {
     uint256 cap = 21000000;
-    constructor() ERC20("NVRToken", "NVR") {
-        _mint(msg.sender, cap * 10 ** decimals());
+    uint256 firstMint = 10500000;
+    constructor(address addr2, address addr3, address addr4, address addr5) ERC20("NVRToken", "NVR") {
+        uint256 addr2TokenAmount = 1050000;
+        uint256 addr3TokenAmount = 140000;
+        uint256 addr4TokenAmount = 140000;
+        uint256 addr5TokenAmount = 140000;
+
+        _mint(msg.sender, firstMint);
+        transfer(addr2, addr2TokenAmount);
+        transfer(addr3, addr3TokenAmount);
+        transfer(addr4, addr4TokenAmount);
+        transfer(addr5, addr5TokenAmount);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         uint256 total = totalSupply();
-        require(total + amount <= cap, "Supply limit reached!" );
+        require(total + amount <= cap, "Token limit reached!" );
         _mint(to, amount);
     }
 }
--- test/NVR.js
+++ test/NVR.js
@@ -1,14 +1,55 @@
 const { expect } = require("chai");
 
-describe.skip("Token contract", function () {
-    it("Deployment should assign the total supply of tokens to the owner", async function () {
-        const [owner] = await ethers.getSigners();
+describe("NVR Token", function () {
+    beforeEach(async function () {
+        accounts = await ethers.getSigners();
+        const addr2 = accounts[1].getAddress();
+        const addr3 = accounts[2].getAddress();
+        const addr4 = accounts[3].getAddress();
+        const addr5 = accounts[4].getAddress();
 
-        const Token = await ethers.getContractFactory("NVR");
-
-        const hardhatToken = await Token.deploy();
-
-        const ownerBalance = await hardhatToken.balanceOf(owner.address);
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+        const NVRToken = await ethers.getContractFactory("NVR");
+        token = await NVRToken.connect(accounts[0]).deploy(addr2, addr3, addr4, addr5);
+        await token.deployed();
     });
+    describe("Ownable", function () {
+        it("It should let owners mint", async function () {
+            const owner = accounts[0];
+            const mintAmout = 1000;
+            let originalBalance = await token.balanceOf(owner.getAddress());
+            const mint = await token.mint(owner.getAddress(), mintAmout);
+
+            let currentBalance = await token.balanceOf(owner.getAddress());
+            expect(currentBalance).to.equal(BigInt(originalBalance) + BigInt(mintAmout));
+
+        });
+        it("It should prevent non-owners from minting", async function () {
+            const address = await accounts[1].getAddress();
+            const mintAmout = 1000;
+            await expect(token.connect(accounts[1]).mint(address, mintAmout)).to.be.reverted
+        });
+    });
+    describe("Mint Transfer", function () {
+        it("Address 2 should have a balance of 1,050,000", async function () {
+            const addr2 = accounts[1].getAddress();
+            const addr2Balance = 1050000;
+            expect(await token.balanceOf(addr2)).to.equal(addr2Balance);
+        });
+        it("Address 3 should have a balance of 140,000", async function () {
+            const addr3 = accounts[2].getAddress();
+            const addr3Balance = 140000;
+            expect(await token.balanceOf(addr3)).to.equal(addr3Balance);
+        });
+        it("Address 4 should have a balance of 140,000", async function () {
+            const addr4 = accounts[3].getAddress();
+            const addr4Balance = 140000;
+            expect(await token.balanceOf(addr4)).to.equal(addr4Balance);
+        });
+        it("Address 5 should have a balance of 140,000", async function () {
+            const addr5 = accounts[4].getAddress();
+            const addr5Balance = 140000;
+            expect(await token.balanceOf(addr5)).to.equal(addr5Balance);
+        });
+    });
+
 });
